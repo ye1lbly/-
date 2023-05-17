7f67022796214d2279161fc64683f60c97bf34dc
syr <blossomy1d@gmail.com>
2023-05-17 22:36:38 +0900

    자바의 정석 2회독 - 6장 연습문제 풀이 중

--- src/practice/Practice6.java
+++ src/practice/Practice6.java
@@ -5,11 +5,38 @@ class Student {
     int no, kor, eng, math; //번호, 국어 영어 수학점수
     static int ban = 1; //반
 
+    Student() {}
     int getTotal() {
         return kor + eng + math;
     }
     float getAverage() {
         return Math.round((getTotal() / 3f) * 10) / 10f;
+    }
+
+    Student(String nm, int b, int n, int k, int e, int m) {
+        name = nm;
+        ban = b;
+        no = n;
+        kor = k;
+        eng = e;
+        math = m;
+    }
+    String info() {
+        return name + ',' + ban + ',' + no + ',' + kor + ',' + eng + ',' + math + ',' + getTotal() + ',' + getAverage();
+    }
+}
+
+class MyPoint {
+    int x, y;
+
+    MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    double getDistance2(int a, int b) {
+        double d = (x - a) * (x - a) + (y - b) * (y - b);
+        return Math.sqrt(d);
     }
 }
 
@@ -29,7 +56,22 @@ public class Practice6 {
         return true;
     }
 
+    static int[] shuffle(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = (int) (Math.random() * arr.length);
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
+        /* 6-2 */
+        Student student = new Student("HGD", 1, 1, 100, 60, 76);
+        String string = student.info();
+        System.out.println(string);
+
         /* 6-3 */
         Student s = new Student();
         s.name = "홍길동";
@@ -44,6 +86,17 @@ public class Practice6 {
 
         /* 6-4 */
         System.out.println(getDistance(1, 1, 2, 2));
+
+        /* 6-6 */
+        MyPoint p = new MyPoint(1, 1);
+        System.out.println(p.getDistance2(2, 2));
+
+        /* 6-17 */
+        int[] original = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        System.out.println(java.util.Arrays.toString(original));
+
+        int[] result = shuffle(original);
+        System.out.println(java.util.Arrays.toString(result));
 
         /* 6-18 */
         String str = "123";
